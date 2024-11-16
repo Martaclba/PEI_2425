9785e9a7d9ae8cac4b22e855ca90f0a7c1427a57
Maria <miniimi2001@gmail.com>
2024-11-16 13:02:45 +0000

    melhorado i think, antes tinha mt overhead

--- code/database/batch_load_file_hmr.py
+++ code/database/batch_load_file_hmr.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import psycopg2
-from psycopg2 import sql
 import tqdm
 import random
 
@@ -25,106 +24,122 @@ with psycopg2.connect(conn_string) as conn:
     with conn.cursor() as cur:
         for df in tqdm.tqdm(df_chunks):
             try:
-                data_batch = []
-                for _, row in df.iterrows():
-                    # Prepare data for batch insert
+                # Create lists to hold data for batch inserts
+                delegate_batch = []
+                region_batch = []
+                district_batch = []
+                town_batch = []
+                company_batch = []
+                product_batch = []
+                hmr_zone_batch = []
+                sale_batch = []
+                sale_product_batch = []
 
-                    # Check and insert into `Delegate`
+                # Process each row in the chunk
+                for _, row in df.iterrows():
+                    # Prepare data for batch inserts
+
+                    # Delegate processing
+                    delegate_id = None
                     cur.execute("SELECT id_Delegate FROM delegate WHERE name = %s", (row['DIM'].strip(),))
                     delegate = cur.fetchone()
                     if delegate is None:
-                        cur.execute("INSERT INTO delegate (name) VALUES (%s) RETURNING id_Delegate", (row['DIM'].strip(),))
-                        delegate = cur.fetchone()
-                    delegate_id = delegate[0]
+                        delegate_batch.append((row['DIM'].strip(),))
+                    else:
+                        delegate_id = delegate[0]
 
-                    # Check and insert into `Region`
+                    # Region processing
+                    region_id = None
                     cur.execute("SELECT id_Region FROM region WHERE name = %s", (row['Region HMR'].strip(),))
                     region = cur.fetchone()
                     if region is None:
-                        cur.execute("INSERT INTO region (name) VALUES (%s) RETURNING id_Region", (row['Region HMR'].strip(),))
-                        region = cur.fetchone()
-                    region_id = region[0]
+                        region_batch.append((row['Region HMR'].strip(),))
+                    else:
+                        region_id = region[0]
 
-                    # Check and insert into `District`
+                    # District processing
+                    district_id = None
                     cur.execute("SELECT id_District FROM district WHERE name = %s", (row['District'].strip(),))
                     district = cur.fetchone()
                     if district is None:
-                        cur.execute("INSERT INTO district (name) VALUES (%s) RETURNING id_District", (row['District'].strip(),))
-                        district = cur.fetchone()
-                    district_id = district[0]
+                        district_batch.append((row['District'].strip(),))
+                    else:
+                        district_id = district[0]
 
-                    # Check and insert into `Town` if applicable
+                    # Town processing (if applicable)
                     town_id = None
                     if pd.notnull(row['Parish']) and row['Parish'].strip():
                         cur.execute("SELECT id_Town FROM town WHERE name = %s", (row['Parish'].strip(),))
                         town = cur.fetchone()
                         if town is None:
-                            cur.execute("INSERT INTO town (name) VALUES (%s) RETURNING id_Town", (row['Parish'].strip(),))
-                            town = cur.fetchone()
-                        town_id = town[0]
+                            town_batch.append((row['Parish'].strip(),))
+                        else:
+                            town_id = town[0]
 
-                    # Check and insert into `Company`
+                    # Company processing
+                    company_id = None
                     cur.execute("SELECT id_Company FROM company WHERE name = %s", (row['Company'].strip(),))
                     company = cur.fetchone()
                     if company is None:
-                        cur.execute("INSERT INTO company (name) VALUES (%s) RETURNING id_Company", (row['Company'].strip(),))
-                        company = cur.fetchone()
-                    company_id = company[0]
+                        company_batch.append((row['Company'].strip(),))
+                    else:
+                        company_id = company[0]
 
-                    # Check and insert into `Product`
-                    cur.execute("SELECT CNP FROM product WHERE name = %s AND fk_id_Company = %s", (row['Product'].strip(), company_id))
+                    # Product processing
+                    product_id = None
+                    cur.execute("SELECT CNP FROM product WHERE name = %s AND fk_id_Company = %s", 
+                                (row['Product'].strip(), company_id))
                     product = cur.fetchone()
                     if product is None:
-                        cur.execute("INSERT INTO product (CNP, name, fk_id_Company) VALUES (%s, %s, %s) RETURNING CNP",
-                                    (random.randint(1, 1000000000), row['Product'].strip(), company_id))
-                        product = cur.fetchone()
-                    product_id = product[0]
+                        product_batch.append((random.randint(1, 1000000000), row['Product'].strip(), company_id))
+                    else:
+                        product_id = product[0]
 
-                    # Check and insert into `HMR_Zone`
+                    # HMR_Zone processing
+                    hmr_zone_id = None
                     cur.execute("SELECT brick FROM hmr_zone WHERE brick = %s", (row['Brick'],))
                     hmr_zone = cur.fetchone()
                     if hmr_zone is None:
-                        cur.execute("""
-                            INSERT INTO hmr_zone (brick, fk_id_Delegate, fk_id_Region, fk_id_District, fk_id_Town)
-                            VALUES (%s, %s, %s, %s, %s) RETURNING brick
-                        """, (row['Brick'], delegate_id, region_id, district_id, town_id))
-                        hmr_zone = cur.fetchone()
-                    hmr_zone_id = hmr_zone[0]
+                        hmr_zone_batch.append((row['Brick'], delegate_id, region_id, district_id, town_id))
+                    else:
+                        hmr_zone_id = hmr_zone[0]
 
-                    # Insert into `Sale` and fetch sale_id
+                    # Sale processing
                     cur.execute("""
                         INSERT INTO sale (registry_date, fk_brick) 
-                        VALUES (%s, %s) RETURNING id_sale  -- Adjusted to match the correct column name
+                        VALUES (%s, %s) RETURNING id_sale
                     """, (row['Date'], row['Brick']))
-                    sale_id = cur.fetchone()[0]  # Get the generated sale_id
+                    sale_id = cur.fetchone()[0]
 
-                    # Prepare row for batch insertion into `Sale`
-                    data_batch.append((
-                        row['Brick'],
-                        product_id,
-                        row['Date'],
-                        row['Value']
-                    ))
+                    # Sale Product processing
+                    product_amount = row.iloc[8]  # Access the 9th column (index 8)
+                    sale_product_batch.append((sale_id, product_id, product_amount))
 
-                    # Insert or retrieve from `sale_product`
-                    product_amount = row.iloc[8]  # Access the 9th column (index 8) using `.iloc[]`
-                    cur.execute("SELECT * FROM sale_product WHERE fk_id_sale = %s AND fk_cnp = %s AND product_amount = %s", 
-                                (sale_id, product_id, product_amount))
-                    sale_product = cur.fetchone()
-                    if sale_product is None:
-                        cur.execute("""
-                            INSERT INTO sale_product (fk_id_sale, fk_cnp, product_amount)
-                            VALUES (%s, %s, %s) RETURNING (fk_id_sale, fk_cnp);
-                        """, (sale_id, product_id, product_amount))
-                        sale_product = cur.fetchone()
+                # Now insert all batch data at once
+                if delegate_batch:
+                    cur.executemany("INSERT INTO delegate (name) VALUES (%s) RETURNING id_Delegate", delegate_batch)
+                if region_batch:
+                    cur.executemany("INSERT INTO region (name) VALUES (%s) RETURNING id_Region", region_batch)
+                if district_batch:
+                    cur.executemany("INSERT INTO district (name) VALUES (%s) RETURNING id_District", district_batch)
+                if town_batch:
+                    cur.executemany("INSERT INTO town (name) VALUES (%s) RETURNING id_Town", town_batch)
+                if company_batch:
+                    cur.executemany("INSERT INTO company (name) VALUES (%s) RETURNING id_Company", company_batch)
+                if product_batch:
+                    cur.executemany("INSERT INTO product (CNP, name, fk_id_Company) VALUES (%s, %s, %s) RETURNING CNP", product_batch)
+                if hmr_zone_batch:
+                    cur.executemany("""
+                        INSERT INTO hmr_zone (brick, fk_id_Delegate, fk_id_Region, fk_id_District, fk_id_Town)
+                        VALUES (%s, %s, %s, %s, %s) RETURNING brick
+                    """, hmr_zone_batch)
+                if sale_product_batch:
+                    cur.executemany("""
+                        INSERT INTO sale_product (fk_id_sale, fk_cnp, product_amount)
+                        VALUES (%s, %s, %s)
+                    """, sale_product_batch)
 
-                # Insert data in batches into `Sale`
-                insert_query = """
-                INSERT INTO sale (registry_date, fk_brick) VALUES (%s, %s)
-                """
-                cur.executemany(insert_query, [(item[2], item[0]) for item in data_batch])
-
-                # Commit the transaction for this batch
+                # Commit the entire transaction for this batch
                 conn.commit()
 
             except Exception as e:
