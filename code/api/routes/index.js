7f712ae24eac380913baccdbe2e5c3b8cd0fafae
Marta Sá <martasa57@hotmail.com>
2024-11-25 19:25:27 +0000

    mudei a ordem das rotas para evitar erros estúpidos...

--- code/api/routes/index.js
+++ code/api/routes/index.js
@@ -34,7 +34,18 @@ const storage = multer.diskStorage({
 // Configure multer upload settings
 const upload = multer({ storage: storage });
 
-/*    POST sales   */
+
+/*    POST delegates (import) */
+// Route responsible for registering multiple delegates via file uploading
+router.post('/import/delegados', function(req, res, next) { 
+  
+  // Queries.createDelegate(res, req) 
+
+});
+
+
+
+/*    POST sales (import)   */
 // Route responsible for sending the hmr file to the database
 router.post('/import/', upload.single('excelFile'), function(req, res, next) {
   if (!req.file) {
@@ -46,6 +57,19 @@ router.post('/import/', upload.single('excelFile'), function(req, res, next) {
 });
 
 
+
+/*    POST visits    */
+// Route responsible for registering a visit
+const visit = {
+  Entidade: 'Médico',
+  Comprador: 2,
+  date: '24-11-2025',
+}
+router.post('/visitas/registar', async function(req, res, next) {
+
+  const visit = req.body
+
+});
 
 /*    GET visits   */
 // Route responsible for retrieving the visits of a delegate (id)
@@ -84,20 +108,6 @@ router.post('/visitas/:id', async function(req, res, next) {
 });
 
 
-/*    POST visits    */
-// Route responsible for registering a visit
-const visit = {
-  Entidade: 'Médico',
-  Comprador: 2,
-  date: '24-11-2025',
-}
-router.post('/visitas/registar', async function(req, res, next) {
-
-  const visit = req.body
-
-});
-
-
 
 /*    Get form's data    */
 // Route responsible for retrieving the form's data for the admin
@@ -110,11 +120,20 @@ router.post('/visitas/registar', async function(req, res, next) {
   // the products
   // doctors
   // pharmacies
-  router.get('/forms', function(req, res, next) { Queries.getSale(res, req) });
-
-  router.get('/forms/:id', function(req, res, next) { Queries.getSale(res, req) });
+router.get('/forms/:id', function(req, res, next) { Queries.getSale(res, req) });
 
 
+router.get('/forms', function(req, res, next) { Queries.getSale(res, req) });
+
+
+
+/*    POST delegates    */
+// Route responsible for registering a delegate
+router.post('/delegados/registar', function(req, res, next) { 
+  
+  // Queries.createDelegate(res, req) 
+
+});
 
 /*    GET delegates   */
 // Route responsible for retrieving the delegates
@@ -197,23 +216,6 @@ router.put('/delegados/detalhes/:idDelegate', function(req, res, next) {
   // Queries.getDelegates(parseInt(req.params.id), res, req) 
 
 });
-
-/*    POST delegates    */
-// Route responsible for registering a delegate
-router.post('/delegados/registar', function(req, res, next) { 
-  
-  // Queries.createDelegate(res, req) 
-
-});
-
-/*    POST delegates    */
-// Route responsible for registering multiple delegates via file uploading
-router.post('/import/delegados', function(req, res, next) { 
-  
-  // Queries.createDelegate(res, req) 
-
-});
-
 
 
 
@@ -318,172 +320,175 @@ router.get('/farmacias/detalhes/:idPharmacy', function(req, res, next) {
 });
 
 
+
 /*    GET sales    */ 
 // Route responsible for retrieving the dashboard's data for each kind of user.
 // Should return a json structured variable with: 
   // the total of sales for each delegate and year as well as the total of sales for every delegate; 
   // the total of sales by product for each delegate,  year (divided in months) and company as well as the tota of sales for every delegate
   // the total of sales by brick for each delegate and year (divided in months) as well as the tota of sales for every delegate
-  router.post('/', async function(req, res, next) { 
-    const data = {
-      histogram: [],
-      products: [],
-      totalProducts: [],
-      bricks: []
-    }
+router.post('/:id', async function(req, res, next) { 
+  const data = {
+    histogram: [],
+    products: [],
+    totalProducts: [],
+    bricks: []
+  }
+
+  const filters = {
+    histogram: {},
+    products: {},
+    totalProducts: {},
+    bricks: {}
+  }
   
-    const filters = {
-      histogram: {},
-      products: {},
-      totalProducts: {},
-      bricks: {}
-    }
-  
-    const default_filter = { label: '-- Todos --', value: 'Todos'}
-  
-    const { type, option_selected } = req.body
-    
-    try {
-      if (type === 'histogram') {
-        const idDelegate = option_selected.Delegate_H                    // default: Todos
-        const year = option_selected.Year_H                              // default: 2024
-  
-        data.histogram = await Queries.getSaleHistogram(idDelegate, year)
-        filters.histogram.delegates = await Queries.getDelegates(year, null, null, null),
-        filters.histogram.years = await Queries.getYears(idDelegate)
-  
-        // Add missing default option 
-        filters.histogram.delegates.unshift(default_filter)
+  const default_filter = { label: '-- Todos --', value: 'Todos'}
+  const year = new Date().getFullYear()
+
+  const { idDelegate } = req.params
+  const { type, option_selected } = req.body
+  // console.log('Type:', type, "Option Select: ", option_selected);
+
+  try {
+    if (options.type === 'histogram') {              
+      data.histogram = await Queries.getSaleHistogram(idDelegate, year)
+      filters.histogram.delegates = await Queries.getDelegates(year, null, null, null),
+      filters.histogram.years = await Queries.getYears(idDelegate)
+
+      // Add missing default option 
+      filters.histogram.delegates.unshift(default_filter)
+
+    } else if (options.type === 'bricks') {
+      const idCompany = option_selected.Company_B                       // default: Todos
+      const idBrick = option_selected.Brick_B                           // default: Todos
+
+      data.bricks = Queries.getSaleBricks(idDelegate, year, idCompany, idBrick) 
+      filters.bricks.companies = Queries.getCompanies(idDelegate, year, idBrick, null)
+      filters.bricks.bricks = Queries.getBricks(idDelegate, year, idCompany, null)  
+
+      data.bricks= await Queries.getSaleBricks(idDelegate, year, idCompany, idBrick)   
+      filters.bricks.companies = await Queries.getCompanies(idDelegate,year,idBrick, null)
+      filters.bricks.bricks = await Queries.getBricks(idDelegate,year,idCompany, null)
+
+      // Add missing default option 
+      filters.bricks.bricks.unshift(default_filter)
+
+    } else if (options.type === 'products') {
+      const idCompany = option_selected.Company_P                        // default: Todos
+      const idBrick = option_selected.Brick_P                            // default: Todos
+      const idProduct = option_selected.Product_P
       
-      } else if (type === 'bricks') {
-          const idDelegate = option_selected.Delegate_B
-          const year = option_selected.Year_B
-          const idCompany = option_selected.Company_B                       // default: Todos
-          const idBrick = option_selected.Brick_B                           // default: Todos
-  
-          data.bricks= await Queries.getSaleBricks(idDelegate, year, idCompany, idBrick)   
-          filters.bricks.delegates = await Queries.getDelegates(year,idCompany,idBrick, null)
-          filters.bricks.years = await Queries.getYears(idDelegate,idCompany,idBrick, null)
-          filters.bricks.companies = await Queries.getCompanies(idDelegate,year,idBrick, null)
-          filters.bricks.bricks = await Queries.getBricks(idDelegate,year,idCompany, null)
-  
-          // Add missing default option 
-          filters.bricks.delegates.unshift(default_filter)
-          filters.bricks.bricks.unshift(default_filter)
+      data.products = await Queries.getSaleProducts(idDelegate, year, idCompany, idBrick, idProduct)
+      filters.products.companies = await Queries.getCompanies(idDelegate,year,idBrick,idProduct)
+      filters.products.bricks = await Queries.getBricks(idDelegate,year,idCompany,idProduct)
+      filters.products.products = await Queries.getProducts(idDelegate,year,idCompany,idBrick)
       
-      } else if (type === 'products') {
-          const idDelegate = option_selected.Delegate_P
-          const year = option_selected.Year_P
-          const idCompany = option_selected.Company_P                        // default: Todos
-          const idBrick = option_selected.Brick_P                            // default: Todos
-          const idProduct = option_selected.Product_P
-          
-          data.products = await Queries.getSaleProducts(idDelegate, year, idCompany, idBrick, idProduct)
-          filters.products.delegates = await Queries.getDelegates(year,idCompany,idBrick,idProduct)
-          filters.products.years = await Queries.getYears(idDelegate,idCompany,idBrick,idProduct)
-          filters.products.companies = await Queries.getCompanies(idDelegate,year,idBrick,idProduct)
-          filters.products.bricks = await Queries.getBricks(idDelegate,year,idCompany,idProduct)
-          filters.products.products = await Queries.getProducts(idDelegate,year,idCompany,idBrick)
-          
-          // Add missing default option 
-          filters.products.delegates.unshift(default_filter)
-          filters.products.bricks.unshift(default_filter)
-          filters.products.products.unshift(default_filter)
+      // Add missing default option 
+      filters.products.bricks.unshift(default_filter)
+      filters.products.products.unshift(default_filter)
+
+    } 
+
+    res.status(200).json({ data, filters });
+  }
+  catch (err) {
+    res.status(501).json({error: err, msg: "Error obtaining sales"});
+  }
+});
+
+router.post('/', async function(req, res, next) { 
+  const data = {
+    histogram: [],
+    products: [],
+    totalProducts: [],
+    bricks: []
+  }
+
+  const filters = {
+    histogram: {},
+    products: {},
+    totalProducts: {},
+    bricks: {}
+  }
+
+  const default_filter = { label: '-- Todos --', value: 'Todos'}
+
+  const { type, option_selected } = req.body
   
-      } else if (type === 'totalProducts') {
-          const idDelegate = option_selected.Delegate_TP
-          const idCompany = option_selected.Company_TP                        // default: Todos
-          const idBrick = option_selected.Brick_TP                            // default: Todos
-          const idProduct = option_selected.Product_TP
-          
-          data.totalProducts = await Queries.getSaleTotalProducts(idDelegate, idCompany, idBrick, idProduct)   
-          filters.totalProducts.delegates = await Queries.getDelegates(null, idCompany,idBrick,idProduct)
-          filters.totalProducts.companies = await Queries.getCompanies(idDelegate, null, idBrick, idProduct)
-          filters.totalProducts.bricks = await Queries.getBricks(idDelegate,null, idCompany, idProduct)
-          filters.totalProducts.products = await Queries.getProducts(idDelegate, null,idCompany,idBrick)
-      
-          // Add missing default option 
-          filters.totalProducts.delegates.unshift(default_filter)
-          filters.totalProducts.bricks.unshift(default_filter)
-          filters.totalProducts.products.unshift(default_filter)
-      }
-        
-      res.status(200).json({ data, filters });
-    }
-    catch (err) {
-      res.status(501).json({error: err, msg: "Error obtaining sales"});
-    }
-  });
+  try {
+    if (type === 'histogram') {
+      const idDelegate = option_selected.Delegate_H                    // default: Todos
+      const year = option_selected.Year_H                              // default: 2024
+
+      data.histogram = await Queries.getSaleHistogram(idDelegate, year)
+      filters.histogram.delegates = await Queries.getDelegates(year, null, null, null),
+      filters.histogram.years = await Queries.getYears(idDelegate)
+
+      // Add missing default option 
+      filters.histogram.delegates.unshift(default_filter)
     
-    
-  router.post('/:id', async function(req, res, next) { 
-    const data = {
-      histogram: [],
-      products: [],
-      totalProducts: [],
-      bricks: []
-    }
-  
-    const filters = {
-      histogram: {},
-      products: {},
-      totalProducts: {},
-      bricks: {}
-    }
-    
-    const default_filter = { label: '-- Todos --', value: 'Todos'}
-    const year = new Date().getFullYear()
-  
-    const { idDelegate } = req.params
-    const { type, option_selected } = req.body
-    // console.log('Type:', type, "Option Select: ", option_selected);
-  
-    try {
-      if (options.type === 'histogram') {              
-        data.histogram = await Queries.getSaleHistogram(idDelegate, year)
-        filters.histogram.delegates = await Queries.getDelegates(year, null, null, null),
-        filters.histogram.years = await Queries.getYears(idDelegate)
-  
-        // Add missing default option 
-        filters.histogram.delegates.unshift(default_filter)
-  
-      } else if (options.type === 'bricks') {
+    } else if (type === 'bricks') {
+        const idDelegate = option_selected.Delegate_B
+        const year = option_selected.Year_B
         const idCompany = option_selected.Company_B                       // default: Todos
         const idBrick = option_selected.Brick_B                           // default: Todos
-  
-        data.bricks = Queries.getSaleBricks(idDelegate, year, idCompany, idBrick) 
-        filters.bricks.companies = Queries.getCompanies(idDelegate, year, idBrick, null)
-        filters.bricks.bricks = Queries.getBricks(idDelegate, year, idCompany, null)  
-  
+
         data.bricks= await Queries.getSaleBricks(idDelegate, year, idCompany, idBrick)   
+        filters.bricks.delegates = await Queries.getDelegates(year,idCompany,idBrick, null)
+        filters.bricks.years = await Queries.getYears(idDelegate,idCompany,idBrick, null)
         filters.bricks.companies = await Queries.getCompanies(idDelegate,year,idBrick, null)
         filters.bricks.bricks = await Queries.getBricks(idDelegate,year,idCompany, null)
-  
+
         // Add missing default option 
+        filters.bricks.delegates.unshift(default_filter)
         filters.bricks.bricks.unshift(default_filter)
-  
-      } else if (options.type === 'products') {
+    
+    } else if (type === 'products') {
+        const idDelegate = option_selected.Delegate_P
+        const year = option_selected.Year_P
         const idCompany = option_selected.Company_P                        // default: Todos
         const idBrick = option_selected.Brick_P                            // default: Todos
         const idProduct = option_selected.Product_P
-       
+        
         data.products = await Queries.getSaleProducts(idDelegate, year, idCompany, idBrick, idProduct)
+        filters.products.delegates = await Queries.getDelegates(year,idCompany,idBrick,idProduct)
+        filters.products.years = await Queries.getYears(idDelegate,idCompany,idBrick,idProduct)
         filters.products.companies = await Queries.getCompanies(idDelegate,year,idBrick,idProduct)
         filters.products.bricks = await Queries.getBricks(idDelegate,year,idCompany,idProduct)
         filters.products.products = await Queries.getProducts(idDelegate,year,idCompany,idBrick)
         
         // Add missing default option 
+        filters.products.delegates.unshift(default_filter)
         filters.products.bricks.unshift(default_filter)
         filters.products.products.unshift(default_filter)
-  
-      } 
-  
-      res.status(200).json({ data, filters });
+
+    } else if (type === 'totalProducts') {
+        const idDelegate = option_selected.Delegate_TP
+        const idCompany = option_selected.Company_TP                        // default: Todos
+        const idBrick = option_selected.Brick_TP                            // default: Todos
+        const idProduct = option_selected.Product_TP
+        
+        data.totalProducts = await Queries.getSaleTotalProducts(idDelegate, idCompany, idBrick, idProduct)   
+        filters.totalProducts.delegates = await Queries.getDelegates(null, idCompany,idBrick,idProduct)
+        filters.totalProducts.companies = await Queries.getCompanies(idDelegate, null, idBrick, idProduct)
+        filters.totalProducts.bricks = await Queries.getBricks(idDelegate,null, idCompany, idProduct)
+        filters.totalProducts.products = await Queries.getProducts(idDelegate, null,idCompany,idBrick)
+    
+        // Add missing default option 
+        filters.totalProducts.delegates.unshift(default_filter)
+        filters.totalProducts.bricks.unshift(default_filter)
+        filters.totalProducts.products.unshift(default_filter)
     }
-    catch (err) {
-      res.status(501).json({error: err, msg: "Error obtaining sales"});
-    }
-  });
+      
+    res.status(200).json({ data, filters });
+  }
+  catch (err) {
+    res.status(501).json({error: err, msg: "Error obtaining sales"});
+  }
+});
+  
+    
+
 
 
 
